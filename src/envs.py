0caac5c082bdbdab45a8f1ad3acc8a8861fda021
thekoc <robot.koc@outlook.com>
2021-06-15 00:42:52 +0800

    Let all tasks be created by `env.create_task`

--- src/envs.py
+++ src/envs.py
@@ -1,17 +1,24 @@
 import heapq
 from numbers import Number
-from typing import Optional, List, Coroutine
+from typing import Callable, Optional, List, Coroutine
 
 from . import exceptions
 
 
 
 class Task:
-    def __init__(self, coro: Coroutine, wait_until=None, callbacks=None):
+    def __init__(self, coro: Optional[Coroutine], wait_until: Optional[Number] = None, callbacks: Optional[List[Callable]] = []):
+        """Init a task. Don't call it directly.
+
+        Args:
+            coro (Optional[Coroutine]): Coroutine inside the task.
+            wait_until (Optional[Number], optional): When should the task be scheduled. Defaults to None.
+            callbacks (Optional[List[Callable]], optional): Set it to None if return to the parent coroutine is not wanted. Defaults to [].
+        """
         self.coro = coro # type: Coroutine
         self._done = False
         self.wait_until = wait_until
-        self.callbacks = callbacks if callbacks else []
+        self.callbacks = [] if callbacks == [] else callbacks
     
     def step(self):
         env = get_current_env()
@@ -19,8 +26,10 @@ class Task:
             return
         else:
             try:
-                next_task = self.coro.send(None)
-                if next_task:
+                next_task: Task = self.coro.send(None)
+                if not isinstance(next_task, Task):
+                    raise TypeError('Yield value must be a `Task`')
+                if next_task.callbacks is not None:
                     def callback():
                         return env.start_task(self)
                     next_task.callbacks.append(callback)
@@ -48,10 +57,6 @@ def get_current_env():
 def get_active_task():
     return get_current_env()._active_task
 
-class EmptyTask(Task):
-    def __init__(self, wait_until=None, callbacks=None):
-        super().__init__(None, wait_until=wait_until, callbacks=callbacks)
-
 class Environment:
     _current_env: Optional['Environment'] = None
 
@@ -62,17 +67,12 @@ class Environment:
         self._running_tasks: List[Task] = [Task(coro, wait_until=self.now) for coro in coros] # type: list[Task]
         self.prev_env: Optional[Environment] = None
 
-    def start_task(self, task: Task, delay: Optional[Number] = None):
-        if delay is None:
-            task.wait_until = self.now
-        else:
-            task.wait_until = self.now + delay
-
+    def start_task(self, task: Task):
         heapq.heappush(self._running_tasks, task)
         return task
     
-    def create_task(self, coro: Coroutine, wait_until=None, callbacks=None, start=True):
-        task = Task(coro=coro, wait_until=wait_until, callbacks=callbacks)
+    def create_task(self, coro: Coroutine, delay: Optional[Number] = 0, callbacks: Optional[List[Callable]] = [], start: bool = True):
+        task = Task(coro=coro, wait_until=self.now + delay, callbacks=callbacks)
         if start:
             self.start_task(task)
         return task
@@ -80,7 +80,7 @@ class Environment:
     def run(self):
         while self._running_tasks:
             task = heapq.heappop(self._running_tasks)
-            self.now = task.wait_until
+            self.now = max(task.wait_until, self.now)
             self._active_task = task
             task.step()
             if task.done():
@@ -108,16 +108,19 @@ def create_env(coros: List[Coroutine], initial_time: Number = 0):
 async def sleep(delay, env: 'Environment' = None):
     if env is None:
         env = get_current_env()
-    
-    return await env.start_task(EmptyTask(), delay)
+
+    return await env.create_task(None, delay)
 
 
 async def gather(coros, env: 'Environment' = None):
     if env is None:
         env = get_current_env()
+
+    gathering_task = env.create_task(None, start=False)
+
     coro_num = len(coros)
     done_num = 0
-    gathering_task = EmptyTask()
+
     def resume():
         nonlocal done_num
         done_num += 1
@@ -125,7 +128,6 @@ async def gather(coros, env: 'Environment' = None):
             env.start_task(gathering_task)
     
     for coro in coros:
-        task = Task(coro, callbacks=[resume])
-        env.start_task(task)
+        env.create_task(coro, callbacks=[resume])
 
     return await gathering_task
