465bc226e9e461bb716ecd31654af3f40590a52b
thekoc <robot.koc@outlook.com>
2021-06-18 21:11:03 +0800

    Updated gitignore and test_masl

--- tests/test_masl.py
+++ tests/test_masl.py
@@ -10,18 +10,21 @@ from simofld.model import Channel, Node
 import simofld.utils as utils
 from numpy import random as np_random
 
-np_random.seed(5)
-random.seed(1)
+# np_random.seed(5)
+# random.seed(1)
 
 
 class TestMASL(unittest.TestCase):
     def test_main_algorithm(self):
-        channels = [RayleighChannel() for _ in range(1)]
-        nodes = [MobileUser(channels) for _ in range(3)]
+        channels = [RayleighChannel() for _ in range(6)]
+        nodes = [MobileUser(channels) for _ in range(6)]
         cloud_server = CloudServer()
-        with envs.create_env([node.main_loop() for node in nodes], until=500) as env:
+        with envs.create_env([node.main_loop() for node in nodes], until=50) as env:
             env.g.cloud_server = cloud_server
             env.run()
+        
+        for node in nodes:
+            print(node._w.round(1) * 10)
 
 if __name__ == '__main__':
     unittest.main()
