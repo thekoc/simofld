d5b684965e4b92c094254be8286fa89cbc675f63
thekoc <thekoc.z@hotmail.com>
2021-07-01 16:41:03 +0800

    Added some figures

--- tests/test_masl.py
+++ tests/test_masl.py
@@ -9,6 +9,8 @@ from simofld import envs, exceptions
 from simofld.model import Channel, Node
 import simofld.utils as utils
 from numpy import random as np_random
+import numpy as np
+from matplotlib import pyplot as plt
 
 # np_random.seed(5)
 # random.seed(1)
@@ -19,12 +21,17 @@ class TestMASL(unittest.TestCase):
         channels = [RayleighChannel() for _ in range(6)]
         nodes = [MobileUser(channels) for _ in range(6)]
         cloud_server = CloudServer()
-        with envs.create_env([node.main_loop() for node in nodes], until=50) as env:
+        with envs.create_env([node.main_loop() for node in nodes], until=10) as env:
             env.g.cloud_server = cloud_server
             env.run()
         
         for node in nodes:
             print(node._w.round(1) * 10)
+        
+        fig, ax = plt.subplots()
+        for i in range(len(channels) + 1):
+            ax.plot([w[i] for w in nodes[0]._w_history])
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
