1c2d1ee1c8a0781e24e5542547d18ad45726ad99
thekoc <thekoc.z@hotmail.com>
2021-07-01 22:06:01 +0800

    Updates

--- src/simofld/masl.py
+++ src/simofld/masl.py
@@ -15,12 +15,12 @@ from .model import LocalData, Node, Channel, Profile, SimulationEnvironment
 
 SIMULATION_PARAMETERS = {
     # CHART
-    'AP_COVERAGE': 20, # meter
+    'AP_COVERAGE': 50, # meter
     'MOBILE_NUM': 20,
     'MOBILE_ACTIVE_PROBABILITY': 0.9,
     'CHANNEL_NUM': 10,
     'CHANNEL_BANDWITH': 5 * 10**6, # MHz
-    'TRANSMIT_POWER': 100 * 10**-3, # mW
+    'TRANSMIT_POWER': 100 * 10**-6, # mW
     'PATH_LOSS_EXPONENT': 4,
     'BACKGROUND_NOISE': 10**-13, # dBm
     'DATA_SIZE': 5000 * 10**3, # KB
@@ -33,10 +33,10 @@ SIMULATION_PARAMETERS = {
     'COMPUTING_ENERGY_EFFECIENCY': (400 * 10**6, 500 * 10**6, 600 * 10**6), # Megacycles/J
     
     # MOBILE
-    'LEARNING_RATE': 0.03,
+    'LEARNING_RATE': 0.05,
     
     # Channel
-    'CHANNEL_SCALING_FACTOR': 10000,
+    'CHANNEL_SCALING_FACTOR': 100000,
 }
 
 logger = getLogger(__name__)
@@ -44,8 +44,10 @@ logger = getLogger(__name__)
 class MobileUser(Node):
     def __init__(self, channels: List[Channel]) -> None:
         super().__init__()
-        self._x = (1 - random.random()) * SIMULATION_PARAMETERS['AP_COVERAGE'] # Distance to the AP
+        self._x = 20 + (1 - random.random()) * SIMULATION_PARAMETERS['AP_COVERAGE'] # Distance to the AP
+        self._x = 5
         self._w = None
+        self._choice_index = None
         self._w_history = []
         self.lr = SIMULATION_PARAMETERS['LEARNING_RATE']
         self.active_probability = 1 - random.random()
@@ -93,10 +95,55 @@ class MobileUser(Node):
         bandwidth = RayleighChannel.bandwidth
         psi = self._psi(bandwidth)
         sigma_0 = SIMULATION_PARAMETERS['BACKGROUND_NOISE']
-        return (channel_power / (2**psi - 1)) - sigma_0
+        result = (channel_power / (2**psi - 1)) - sigma_0
+        return result
 
     def _I(self, channel: 'RayleighChannel') -> Number:
         return channel.total_channel_power(exclude=self)
+    
+    def _get_cloud_server(self) -> 'CloudServer':
+        cloud_server: CloudServer = self.get_current_env().g.cloud_server
+        return cloud_server
+
+    def cloud_cost(self, channel: Channel) -> Number:
+        mu_E = self._payoff_weight_energy
+        mu_T = self._payoff_weight_time
+        C = self._datasize
+        cloud_server: 'CloudServer' = self.get_current_env().g.cloud_server
+
+        D_clo = SIMULATION_PARAMETERS['CLOUD_CPU_CYCLES']
+        F_clo = cloud_server.cpu_frequency
+        cloud_duration = D_clo / F_clo
+
+        transmission_duration = C / channel.datarate_between(self, self._get_cloud_server())
+
+        total_duration = transmission_duration + cloud_duration
+        total_energy = self.transmit_power * transmission_duration
+
+        return mu_T * total_duration + mu_E * total_energy
+    
+    def local_cost(self) -> Number:
+        mu_E = self._payoff_weight_energy
+        mu_T = self._payoff_weight_time
+        D_loc = SIMULATION_PARAMETERS['LOCAL_CPU_CYCLES']
+        F_loc = self.cpu_frequency
+        T_loc = D_loc / F_loc
+        E_loc = T_loc / self.cpu_effeciency
+        return mu_T * T_loc + mu_E * E_loc
+    
+    def expectation_cost(self) -> Number:
+        weighted_cost = 0
+        assert len(self._w) >= 1
+        assert len(self._w) == len(self.channels) + 1
+        
+        for i, weight in enumerate(self._w):
+            if i == 0:
+                weighted_cost += weight * self.local_cost()
+            else:
+                channel = self.channels[i - 1]
+                weighted_cost += weight * self.cloud_cost(channel)
+        
+        return weighted_cost
 
     async def perform_cloud_computation(self, cloud_server: 'CloudServer', channel: Channel, upload_duration: Number):
         env = self.get_current_env()
@@ -117,11 +164,10 @@ class MobileUser(Node):
 
     async def main_loop(self):
         channel_num = len(self.channels)
-        step_lapse = 0.01
+        step_interval = self.get_current_env().g.step_interval
         lr = self.lr
         choice_list = [None] + self.channels
-        
-        cloud_server: CloudServer = self.get_current_env().g.cloud_server
+        cloud_server = self._get_cloud_server()
 
         self._w = np.full(channel_num + 1, 1 / (channel_num + 1))
         w = self._w
@@ -133,12 +179,13 @@ class MobileUser(Node):
 
             if self.active:
                 choice_index = random.choice(channel_num + 1, 1, p=w).item()
+                self._choice_index = choice_index
 
                 if choice_index == 0: # local execution
                     logger.debug(f'Calculating Q for user {self.id}')
                     ongoing_trans = None
                     payoff = self._Q()
-                    await self.perform_local_computation(step_lapse)
+                    await self.perform_local_computation(step_interval)
                     logger.debug(f'Q: {payoff}')
                     logger.debug(f'I: {self._I(self.channels[0])}')
 
@@ -150,7 +197,7 @@ class MobileUser(Node):
                     payoff = self._I(channel)
                     logger.debug(f'Calculating I for user {self.id}, result:{payoff},  channel: {channel.id} list: {ongoing_trans}')
 
-                    await self.perform_cloud_computation(cloud_server, channel, step_lapse)
+                    await self.perform_cloud_computation(cloud_server, channel, step_interval)
                     logger.debug(f'I: {payoff}')
 
                 e = np.zeros(channel_num + 1)
@@ -170,7 +217,7 @@ class MobileUser(Node):
                 self._w_history.append(w)
                 assert r > 0
             else:
-                await envs.sleep(step_lapse)
+                await envs.sleep(step_interval)
 
 
 class RayleighChannel(Channel):
@@ -180,7 +227,7 @@ class RayleighChannel(Channel):
 
     # TODO: Update it periodically
     beta: Number = random.exponential(1) # Rayleigh fading factor
-
+    beta = 1
     def __init__(self) -> None:
         super().__init__()
 
