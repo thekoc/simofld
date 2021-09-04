df0ac580fca2e2de9cc344a91e585a95838efdb3
thekoc <thekoc.z@hotmail.com>
2021-09-04 17:40:26 +0800

    Fixed a bug

--- src/simofld/masl_deepq.py
+++ src/simofld/masl_deepq.py
@@ -210,7 +210,9 @@ class Profile(MASLProfile):
     def sample(self):
         temp_nodes = []
         for node in self.nodes:
-            if node._run_until is not None and node._run_until < envs.get_current_env().now:
+            if node._run_until is None:
+                temp_nodes.append(node)
+            elif node._run_until < envs.get_current_env().now:
                 temp_nodes.append(node)
         self.nodes = temp_nodes
         print(f'now: {envs.get_current_env().now}')
